b34d90440eef0f781fe7faccb00184596d18a105
João Bispo <joaobispo2077@gmail.com>
2021-08-28 15:54:27 -0300

    feat: add create user & throw error when user already exists

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { username, name } = request.body;
+
+  const isUserAlreadyExists = users.find(user => user.username === username);
+  if (isUserAlreadyExists) {
+    return response.status(400).json({ error: 'Username already exists.' });
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    username,
+    name,
+    todos: []
+  };
+
+  users.push(newUser);
+
+  return response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
